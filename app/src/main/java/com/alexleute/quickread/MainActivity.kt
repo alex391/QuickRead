423fbacee50c91a34e1656e696f31c301ae8fc1a
Alex Leute <alex@alexleute.com>
2024-10-18 03:01:57 -0400

    initial commit

---
+++ app/src/main/java/com/alexleute/quickread/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.alexleute.quickread
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alexleute.quickread.ui.theme.QuickReadTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuickReadTheme {
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    CenterAlignedTopAppBar(
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),
+                        title = {
+                        },
+                        actions =  {
+                            IconButton(onClick = {
+                                TODO()
+                            }) {
+                                Icon(imageVector = Icons.Filled.Menu, contentDescription = null/* TODO */)
+                            }
+                        }
+                    )
+                }) { innerPadding ->
+                    Column (modifier = Modifier.padding(innerPadding)) {
+                        TextField("", onValueChange = {}, label = {
+                            Text("test")
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
